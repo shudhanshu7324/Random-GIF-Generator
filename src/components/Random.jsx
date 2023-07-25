8ca90f84dfef4a49367a175fb973c086d8401bb1
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2023-07-26 00:45:22 +0530

    making code more cleaner

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from 'react'
 import Spinner from './Spinner';
-import axios from 'axios';
 import useGif from '../hooks/useGif';
-
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
 
-    const {gif,loading,fetchData} = useGif();
-
-    function clickHandler(){
-      fetchData();
-    }
+  const {gif,loading,fetchData} = useGif();
 
   return (
     <div className='w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[20px]'>
@@ -21,11 +13,11 @@ const Random = () => {
         loading ? (<Spinner/>):(<img src={gif} alt='random-gif' width="450px" />)
       }
       
-        <button onClick={clickHandler} className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
+        <button onClick = {() => fetchData()} className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
             Generate
         </button>
     </div>
-  )
+  );
 }
 
-export default Random
+export default Random;
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,24 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, {useState } from 'react'
 import Spinner from './Spinner';
 import useGif from '../hooks/useGif';
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-
-
 const Tag = () => {
 
-
     const [tag,setTag] = useState('cars');
-
     const {gif,loading,fetchData} = useGif(tag);
-
-    function clickHandler(){
-      fetchData(tag);
-    }
-
-    function changeHandler(event){
-      setTag(event.target.value);
-    }
 
   return (
     <div className='w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[20px]'>
@@ -30,10 +17,11 @@ const Tag = () => {
       
         <input 
         className='w-10/12 text-lg py-2 rounded-lg '
-        onChange={changeHandler}
+        onChange={(event) => setTag(event.target.value)}
         value={tag}
         />
-        <button onClick={clickHandler} className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
+        <button onClick={() => fetchData(tag) } 
+        className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
             Generate {tag} GIF
         </button>
     </div>
