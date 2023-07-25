acff002d5699d7ae3e37cfece94675ffd3dbda7d
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2023-07-25 20:55:15 +0530

    added loader to the random component

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Spinner from './Spinner';
 import axios from 'axios';
 
 const Random = () => {
@@ -6,13 +7,16 @@ const Random = () => {
     const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
     const [gif,setGif] = useState('');
+    const [loading,setLoading] = useState(false);
 
     async function fetchData(){
+      setLoading(true);
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
       const {data} = await axios.get(url);
       const imageSource = data.data.images.downsized_large.url;
       // console.log(imageSource);
       setGif(imageSource);
+      setLoading(false);
     }
 
     useEffect( () => {
@@ -26,7 +30,11 @@ const Random = () => {
   return (
     <div className='w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[20px]'>
       <h1 className='mt-[20px] text-2xl font-bold underline uppercase'>A Random Gif</h1>
-      <img src={gif} alt='random-gif' width="450px" />
+
+      {
+        loading ? (<Spinner/>):(<img src={gif} alt='random-gif' width="450px" />)
+      }
+      
         <button onClick={clickHandler} className='w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]'>
             Generate
         </button>
